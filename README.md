db4bef6bcff6543cd8296d5501c465f62b96e6fc
JuananCampillo <47898170+JuananCampillo@users.noreply.github.com>
2019-06-15 15:10:40 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MATRIX CAPSULES EM-Tensorflow
 
 [![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg?style=plastic)](https://opensource.org/licenses/Apache-2.0)
-![completion](https://img.shields.io/badge/completion%20state-95%25-blue.svg?style=plastic)
+![completion](https://img.shields.io/badge/completion%20state-70%25-blue.svg?style=plastic)
 
 A Tensorflow implementation of CapsNet based on paper [Matrix Capsules with EM Routing](https://openreview.net/pdf?id=HJWLfGWRb)
 
@@ -11,30 +11,17 @@ A Tensorflow implementation of CapsNet based on paper [Matrix Capsules with EM R
 > 3. Some modification and optimization is implemented to prompt the numerical stability of GMM. Specific explanations can be found in the code.
 > 4. With configuration A=32, B=4, D=4, D=4, batch_size=128, each iteration of training takes around 0.6s on a Tesla P40 GPU.
 
-> **Current Results on smallNORB:**
-- Configuration: A=32, B=8, C=16, D=16, batch_size=50, iteration number of EM routing: 2, with Coordinate Addition, spread loss, batch normalization
-- Training loss. Variation of loss is suppressed by batch normalization. However there still exists a gap between our best results and the reported results in the original paper.
-![spread loss](imgs/spread_loss_norb.png)
-
-- Test accuracy(current best result is 91.8%)
-![test_acc](imgs/test_accuracy_norb.png)
-
-> **Ablation Study on smallNORB:**
-- Configuration: A=32, B=8, C=16, D=16, batch_size=32, iteration number of EM routing: 2, with Coordinate Addition, spread loss, test accuracy is 79.8%.
-
 > **Current Results on MNIST:**
-- Configuration: A=32, B=8, C=16, D=16, batch_size=50, iteration number of EM routing: 2, with Coordinate Addition, spread loss, batch normalization, reconstruction loss.
+- Configuration: A=32, B=4, C=4, D=4, batch_size=128, iteration number of EM routing: 2, with Coordinate Addition, cross entropy loss
 
-- Training loss.
-![spread loss](imgs/training_loss.png)
+- Training loss
+![cross entropy loss](imgs/training_loss.png)
 
-- Test accuracy(current best result is 99.3%, only 10% samples are used in test)
+- Test accuracy(current best result is 96.80%)
 ![test_acc](imgs/test_accuracy.png)
 
-> **Ablation Study on MNIST:**
+> **Ablation Study:**
 - Configuration: A=32, B=4, C=4, D=4, batch_size=128, iteration number of EM routing: 2, no Coordinate Addition, cross entropy loss, test accuracy is 96.4%.
-- Configuration: A=32, B=4, C=4, D=4, batch_size=128, iteration number of EM routing: 2, with Coordinate Addition, cross entropy loss, test accuracy is 96.8%.
-- Configuration: A=32, B=8, C=16, D=16, batch_size=32, iteration number of EM routing: 2, with Coordinate Addition, spread loss 99.1%.
 
 > **To Do List:**
 > 1. Experiments on smallNORB as in paper is about to be casted.
@@ -45,9 +32,6 @@ Any questions and comments to the code and the original algorithms are welcomed!
 - Python >= 3.4
 - Numpy
 - Tensorflow >= 1.2.0
-- Keras
-
-```pip install -r requirement.txt```
 
 ## Usage
 **Step 1.**
@@ -67,57 +51,31 @@ $ wget -c -P data/mnist http://yann.lecun.com/exdb/mnist/{train-images-idx3-ubyt
 $ gunzip data/mnist/*.gz
 ```
 
-***To install smallNORB, follow instructions in ```./data/README.md```***
-
 **Step 3.**
-Start the training(MNIST):
+Start the training:
 ```
-$ python3 train.py "mnist"
-```
-
-**Step 4.**
-Download the [Fashion MNIST dataset](https://github.com/zalandoresearch/fashion-mnist), ``mv`` and extract it into ``data/fashion_mnist`` directory.(Be careful the backslash appeared around the curly braces when you copy the ``wget `` command to your terminal, remove it)
-
-```
-$ mkdir -p data/fashion_mnist
-$ wget -c -P data/fashion_mnist http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/{train-images-idx3-ubyte.gz,train-labels-idx1-ubyte.gz,t10k-images-idx3-ubyte.gz,t10k-labels-idx1-ubyte.gz}
-$ gunzip data/fashion_mnist/*.gz
-```
-
-Start the training(smallNORB):
-```
-$ python3 train.py "smallNORB"
-```
-
-Start the training(CNN baseline):
-```
-$ python3 train_baseline.py "smallNORB"
+$ python3 train.py
 ```
 
 **Step 4.**
 View the status of training:
 ```
-$ tensorboard --logdir=./logdir/{model_name}/{dataset_name}/train_log/
-```
-Open the url tensorboard has shown.
+$ tensorboard --logdir=./logdir
+``` 
+Open a chrome browser, visit the site: http://127.0.0.1:6006/
 
 **Step 5.**
 Start the test on MNIST:
 ```
-$ python3 eval.py "mnist" "caps"
-```
-
-Start the test on smallNORB:
-```
-$ python3 eval.py "smallNORB" "caps"
+$ python3 eval.py
 ```
 
 **Step 6.**
 View the status of test:
 ```
-$ tensorboard --logdir=./test_logdir/{model_name}/{dataset_name}/
+$ tensorboard --logdir=./test_logdir
 ```
-Open the url tensorboard has shown.
+Open a chrome browser, visit the site: http://127.0.0.1:6006/
 
 ### Reference
 - [naturomics/CapsNet-Tensorflow](https://github.com/naturomics/CapsNet-Tensorflow): the implementation of Hinton's paper [Dynamic Routing Between Capsules](https://arxiv.org/abs/1710.09829)
--- train.py
+++ train.py
@@ -1,155 +1,91 @@
 """
 License: Apache-2.0
-Author: Suofei Zhang | Hang Yu
-E-mail: zhangsuofei at njupt.edu.cn | hangyu5 at illinois.edu
+Author: Suofei Zhang
+E-mail: zhangsuofei at njupt.edu.cn
 """
 
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from config import cfg, get_coord_add, get_dataset_size_train, get_num_classes, get_create_inputs
+from config import cfg
+from utils import create_inputs, create_inputs_norb
 import time
 import numpy as np
-import sys
 import os
 import capsnet_em as net
 
-import logging
-import daiquiri
 
-daiquiri.setup(level=logging.DEBUG)
-logger = daiquiri.getLogger(__name__)
+def main(_):
+    coord_add = [[[8., 8.], [12., 8.], [16., 8.]],
+                 [[8., 12.], [12., 12.], [16., 12.]],
+                 [[8., 16.], [12., 16.], [16., 16.]]]
 
-
-def main(args):
-    """Get dataset hyperparameters."""
-    assert len(args) == 2 and isinstance(args[1], str)
-    dataset_name = args[1]
-    logger.info('Using dataset: {}'.format(dataset_name))
-
-    """Set reproduciable random seed"""
-    tf.set_random_seed(1234)
-
-    coord_add = get_coord_add(dataset_name)
-    dataset_size = get_dataset_size_train(dataset_name)
-    num_classes = get_num_classes(dataset_name)
-    create_inputs = get_create_inputs(dataset_name, is_train=True, epochs=cfg.epoch)
+    coord_add = np.array(coord_add, dtype=np.float32) / 28.
 
     with tf.Graph().as_default(), tf.device('/cpu:0'):
-        """Get global_step."""
         global_step = tf.get_variable(
             'global_step', [], initializer=tf.constant_initializer(0), trainable=False)
 
-        """Get batches per epoch."""
-        num_batches_per_epoch = int(dataset_size / cfg.batch_size)
+        num_batches_per_epoch = int(60000 / cfg.batch_size)
+        opt = tf.train.AdamOptimizer()
 
-        """Use exponential decay leanring rate?"""
-        lrn_rate = tf.maximum(tf.train.exponential_decay(
-            1e-3, global_step, num_batches_per_epoch, 0.8), 1e-5)
-        tf.summary.scalar('learning_rate', lrn_rate)
-        opt = tf.train.AdamOptimizer()  # lrn_rate
-
-        """Get batch from data queue."""
-        batch_x, batch_labels = create_inputs()
+        batch_x, batch_labels = create_inputs_norb(is_train=True)
         # batch_y = tf.one_hot(batch_labels, depth=10, axis=1, dtype=tf.float32)
 
-        """Define the dataflow graph."""
         m_op = tf.placeholder(dtype=tf.float32, shape=())
         with tf.device('/gpu:0'):
             with slim.arg_scope([slim.variable], device='/cpu:0'):
-                batch_squash = tf.divide(batch_x, 255.)
-                batch_x = slim.batch_norm(batch_x, center=False, is_training=True, trainable=True)
-                output, pose_out = net.build_arch(batch_x, coord_add, is_train=True,
-                                                  num_classes=num_classes)
+                output = net.build_arch(batch_x, coord_add, is_train=True)
                 # loss = net.cross_ent_loss(output, batch_labels)
-                tf.logging.debug(pose_out.get_shape())
-                loss, spread_loss, mse, _ = net.spread_loss(
-                    output, pose_out, batch_squash, batch_labels, m_op)
-                acc = net.test_accuracy(output, batch_labels)
-                tf.summary.scalar('spread_loss', spread_loss)
-                tf.summary.scalar('reconstruction_loss', mse)
-                tf.summary.scalar('all_loss', loss)
-                tf.summary.scalar('train_acc', acc)
+                loss = net.spread_loss(output, batch_labels, m_op)
 
-            """Compute gradient."""
             grad = opt.compute_gradients(loss)
-            # See: https://stackoverflow.com/questions/40701712/how-to-check-nan-in-gradients-in-tensorflow-when-updating
-            grad_check = [tf.check_numerics(g, message='Gradient NaN Found!')
-                          for g, _ in grad if g is not None] + [tf.check_numerics(loss, message='Loss NaN Found')]
 
-        """Apply graident."""
-        with tf.control_dependencies(grad_check):
-            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-            with tf.control_dependencies(update_ops):
-                train_op = opt.apply_gradients(grad, global_step=global_step)
+        loss_name = 'spread_loss'
 
-        """Set Session settings."""
+        summaries = []
+        summaries.append(tf.summary.scalar(loss_name, loss))
+
+        train_op = opt.apply_gradients(grad, global_step=global_step)
+
         sess = tf.Session(config=tf.ConfigProto(
             allow_soft_placement=True, log_device_placement=False))
-        sess.run(tf.local_variables_initializer())
         sess.run(tf.global_variables_initializer())
 
-        """Set Saver."""
-        var_to_save = [v for v in tf.global_variables(
-        ) if 'Adam' not in v.name]  # Don't save redundant Adam beta/gamma
-        saver = tf.train.Saver(var_list=var_to_save, max_to_keep=cfg.epoch)
-
-        """Display parameters"""
-        total_p = np.sum([np.prod(v.get_shape().as_list()) for v in var_to_save]).astype(np.int32)
-        train_p = np.sum([np.prod(v.get_shape().as_list())
-                          for v in tf.trainable_variables()]).astype(np.int32)
-        logger.info('Total Parameters: {}'.format(total_p))
-        logger.info('Trainable Parameters: {}'.format(train_p))
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=cfg.epoch)
 
         # read snapshot
         # latest = os.path.join(cfg.logdir, 'model.ckpt-4680')
         # saver.restore(sess, latest)
-        """Set summary op."""
-        summary_op = tf.summary.merge_all()
 
-        """Start coord & queue."""
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        summary_op = tf.summary.merge(summaries)
+        tf.train.start_queue_runners(sess=sess)
 
-        """Set summary writer"""
-        if not os.path.exists(cfg.logdir + '/caps/{}/train_log/'.format(dataset_name)):
-            os.makedirs(cfg.logdir + '/caps/{}/train_log/'.format(dataset_name))
-        summary_writer = tf.summary.FileWriter(
-            cfg.logdir + '/caps/{}/train_log/'.format(dataset_name), graph=sess.graph)  # graph = sess.graph, huge!
+        summary_writer = tf.summary.FileWriter(cfg.logdir, graph=sess.graph)
 
-        """Main loop."""
-        m_min = 0.2
-        m_max = 0.9
-        m = m_min
-        for step in range(cfg.epoch * num_batches_per_epoch + 1):
+        m = 0.2
+        for step in range(cfg.epoch * num_batches_per_epoch):
             tic = time.time()
-            """"TF queue would pop batch until no file"""
-            try:
-                _, loss_value, summary_str = sess.run(
-                    [train_op, loss, summary_op], feed_dict={m_op: m})
-                logger.info('%d iteration finishs in ' % step + '%f second' %
-                            (time.time() - tic) + ' loss=%f' % loss_value)
-            except KeyboardInterrupt:
-                sess.close()
-                sys.exit()
-            except tf.errors.InvalidArgumentError:
-                logger.warning('%d iteration contains NaN gradients. Discard.' % step)
-                continue
-            else:
-                """Write to summary."""
-                if step % 5 == 0:
-                    summary_writer.add_summary(summary_str, step)
+            _, loss_value = sess.run([train_op, loss], feed_dict={m_op: m})
+            print('%d iteration is finished in ' % step + '%f second' % (time.time() - tic))
+            # test1_v = sess.run(test2)
 
-                """Epoch wise linear annealling."""
-                if (step % num_batches_per_epoch) == 0:
-                    if step > 0:
-                        m += (m_max - m_min) / (cfg.epoch * cfg.m_schedule)
-                        if m > m_max:
-                            m = m_max
+            # if np.isnan(loss_value):
+            #     print('bbb')
+            #  assert not np.isnan(np.any(test2_v[0])), 'a is nan'
+            assert not np.isnan(loss_value), 'loss is nan'
 
-                    """Save model periodically"""
-                    ckpt_path = os.path.join(
-                        cfg.logdir + '/caps/{}/'.format(dataset_name), 'model-{:.4f}.ckpt'.format(loss_value))
-                    saver.save(sess, ckpt_path, global_step=step)
+            if step % 10 == 0:
+                summary_str = sess.run(summary_op, feed_dict={m_op: m})
+                summary_writer.add_summary(summary_str, step)
+
+            if (step % num_batches_per_epoch) == 0:
+                if step > 0:
+                    m += (0.9 - 0.2) / (cfg.epoch * 0.6)
+                    if m > 0.9:
+                        m = 0.9
+
+                ckpt_path = os.path.join(cfg.logdir, 'model.ckpt')
+                saver.save(sess, ckpt_path, global_step=step)
 
 
 if __name__ == "__main__":
